2ca795e39cf0c1e55afe4c21204a2c0a78468eb3
Vinícius Soares <viniciussoaress1998@gmail.com>
2023-01-07 19:34:59 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidarCPF
+{
+	class Program
+	{
+		public static void Main()
+		{
+			string cpf;
+			bool validar;
+			char opcao;
+			cpf:
+			Console.WriteLine("Digite seu CPF:(Somente os números!)");
+			cpf = Console.ReadLine();
+
+			ValidacaoCPF validacaoCPF = new ValidacaoCPF();
+			validar = validacaoCPF.ValidaCpf(cpf);
+
+			if (validar == true)
+			{
+				Console.WriteLine("CPF Válido!");
+				Console.ReadKey();
+				Console.Clear();
+				Console.WriteLine("Fechando sistema!");
+				Console.ReadKey();
+			}
+			else
+			{
+				Console.WriteLine("CPF Inválido!");
+				Console.WriteLine("Gostaria de Digitar novamente?(S/N)");
+				opcao = char.Parse(Console.ReadLine());
+				Console.Clear();
+				while (opcao == 'S' || opcao == 's')
+				{
+					goto cpf;
+				}
+				Console.Clear();
+				Console.WriteLine("Fechando sistema!");
+				Console.ReadKey();
+			}
+
+		}
+	}
+}
---
+++ ValidaCPF.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidarCPF
+{
+	public class ValidacaoCPF
+	{
+		public bool ValidaCpf(string cpf)
+		{
+
+			int pos, cont = 10, soma = 0, numero, result, digito1, digito2;
+
+
+			if (cpf.Length > 11 || cpf.Length < 11)
+			{
+				return (false);
+			}
+			else
+			{
+
+				if (cpf != "11111111111" && cpf != "22222222222" && cpf != "33333333333" && cpf != "44444444444" && cpf != "55555555555" && cpf != "66666666666" && cpf != "77777777777" && cpf != "88888888888" && cpf != "99999999999" && cpf != "00000000000")
+				{
+
+					for (pos = 0; pos < cpf.Length - 2; pos++)
+					{
+
+						numero = (int.Parse(cpf[pos].ToString()));
+						soma = soma + numero * cont;
+						cont--;
+
+					}
+
+					result = 11 - (soma % 11);
+
+					if (result >= 10)
+					{
+						digito1 = 0;
+					}
+					else
+					{
+						digito1 = result;
+					}
+
+					if (digito1 == (int.Parse(cpf[9].ToString())))
+					{
+						return (true);
+					}
+					else
+						return (false);
+
+					cont = 11;
+					soma = 0;
+					for (pos = 0; pos < cpf.Length - 1; pos++)
+					{
+						numero = (int.Parse(cpf[pos].ToString()));
+						soma = soma + numero * cont;
+						cont--;
+					}
+
+					result = 11 - (soma % 11);
+
+					if (result >= 10)
+					{
+						digito2 = 0;
+					}
+					else
+					{
+						digito2 = result;
+					}
+
+					if (digito2 == (int.Parse(cpf[10].ToString())))
+					{
+						return (true);
+					}
+					else   
+						return (false);
+				}
+				else
+					return (false);
+			}
+		}
+	}
+}
